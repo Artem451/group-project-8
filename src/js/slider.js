295a370de5206a59e1dd136a5dbfd05e87ff183b
Ihor Pshenychnyi <igorynia88@gmail.com>
2021-03-15 00:03:08 +0200

    Добавил подключение слайдера

---
+++ src/js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        speed: 500,
+        easing: 'ease',
+        adaptiveHeight: false,
+        infinite: false,
+        draggable: false
+    });
+});
